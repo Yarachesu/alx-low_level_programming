d260a6d433dea31faef1fa8c57210778d2ecbbb8
myusername <myemail@gmail.com>
2023-02-16 09:56:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *  main - this is to print line
+ *  Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
