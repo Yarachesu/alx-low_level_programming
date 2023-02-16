223ab298a6043a9607509a091d16bdddd5083d32
myusername <myemail@gmail.com>
2023-02-16 10:40:21 -0800

    thank you

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@ int main(void)
 	char y;
 	int d;
 	long int r;
-	float f;
 	long long int l;
+	float f;
 printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(y));
 printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(d));
 printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(r));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(l));
+printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }
