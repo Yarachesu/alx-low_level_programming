6352678fe029764899bfd870a3f4057e27137639
myusername <myemail@gmail.com>
2023-02-17 07:39:52 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - lowercase and uppercase
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'z'; ch++)
+{
+putchar('\n');
+}
+putchar('\n');
+return (0);
