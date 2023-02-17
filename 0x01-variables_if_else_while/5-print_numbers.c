16b619f53a05c9b9b7de232265bdf9cf7d736d94
myusername <myemail@gmail.com>
2023-02-17 08:41:54 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - the program to print the number 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
