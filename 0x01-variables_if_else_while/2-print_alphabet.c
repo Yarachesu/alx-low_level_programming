89debca1cf38bc1bae92b6c79d7e7e402005e9e2
myusername <myemail@gmail.com>
2023-02-17 07:29:46 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print alpa in lower case
+ * Return: 0
+ */
+int main(void)
+{
+char Lc;
+for (Lc = 'a'; Lc <= 'z'; Lc++)
+{
+putchar(Lc);
+}
+putchar('\n');
+return (0);
+}
+
