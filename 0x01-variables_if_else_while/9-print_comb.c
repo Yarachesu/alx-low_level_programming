4a32d0d52efa91a74f41b64b3d5ee7d95057c15c
myusername <myemail@gmail.com>
2023-02-17 09:30:11 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - the program to print all combination of single digit
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putuchar(a);
+if (a != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
