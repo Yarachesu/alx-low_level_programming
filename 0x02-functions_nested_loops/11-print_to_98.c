46b9b12a0c1c5ece970123be0c25805f24bae33a
myusername <myemail@gmail.com>
2023-02-21 13:22:08 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * print_to_98 - print
+ * print_to_98 - print all member from input to 98
  */
 void print_to_98(int n)
 {
 if (n >= 98)
 {
-while (n >98)
+while (n > 98)
 {
 printf("%d, ", n--);
 }
